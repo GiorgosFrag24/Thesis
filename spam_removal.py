32ea6b0b12bb513cec129d9d56fe1f61937530f3
Giorgos Frag <polekespidi@gmail.com>
2021-11-11 01:56:51 +0200

    Update spam_removal.py

--- spam_removal.py
+++ spam_removal.py
@@ -8,12 +8,9 @@ notify = Notify()
 Spam_Accounts = defaultdict(dict) # A dictionary containing all the spam accounts
 
 
-rapidapi_key = "ebc81ab30fmsh3ff7cd41c4ee3f9p1d564cjsn1e6fb4d1ca51" # now it's called rapidapi key
+rapidapi_key = ...
 twitter_app_auth = {
-	'consumer_key': 'XLfRlXSVKIpH28wahBwVBbPCy',
-	'consumer_secret': 'PFsLz8P4HppMKOglDginEFxWUxZH6Ghfem3qg6FSu2xxFUwnEK',
-	'access_token': '1156146192448917504-M9Hss2TbG5YOR7FC9d2QtK1DPA5J5B',
-	'access_token_secret': 'khVPFfyK0B4lJCDPW86Vup4dN5IiI2F2SpC0GbevQrEnB',
+	...
   }
 bom = botometer.Botometer(wait_on_ratelimit=True,
 						  rapidapi_key=rapidapi_key,
